00a28167b3dc6e323d78eb66ba49bd9a449e0d17
erikbg7 <erik.bg7@gmail.com>
2022-07-30 18:23:04 +0200

    Basic trpc setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { trpc } from '../utils/trpc';
 
 const Home: NextPage = () => {
+  const { data, isLoading } = trpc.useQuery(['hello', { text: 'morty' }]);
+
+  if (isLoading) {
+    return <div>Loading....</div>;
+  }
+
+  if (data) {
+    return <div>{data.greeting}</div>;
+  }
+
   return (
     <div>
       <Head>
