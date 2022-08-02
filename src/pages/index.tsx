35b08a0f234d785ce10127fe54bdb56082b8d4d4
erikbg7 <erik.bg7@gmail.com>
2022-08-02 16:52:14 +0200

    TRPC route to use new schema

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,7 +60,6 @@ type CharacterCardProps = { id: string; onClick(id: string): void };
 
 const CharacterCard: React.FC<CharacterCardProps> = ({ id, onClick }) => {
   const { data, isLoading } = trpc.useQuery(['get-character-by-id', { id }]);
-  
   if (isLoading) {
     return <div></div>;
   }
@@ -72,14 +71,14 @@ const CharacterCard: React.FC<CharacterCardProps> = ({ id, onClick }) => {
         onClick={() => onClick(id)}
       >
         <Image
-          alt={data.character.name}
-          src={data.character.image}
+          alt={data!.character!.name}
+          src={data!.character!.imageUrl}
           layout="fill"
           placeholder="blur"
           blurDataURL="data:image/svg+xml;base64,Cjxzdmcgd2lkdGg9IjQwMCIgaGVpZ2h0PSIxNDAiIHZlcnNpb249IjEuMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayI+CiAgPGRlZnM+CiAgICA8bGluZWFyR3JhZGllbnQgaWQ9ImciPgogICAgICA8c3RvcCBzdG9wLWNvbG9yPSIjMzMzIiBvZmZzZXQ9IjIwJSIgLz4KICAgICAgPHN0b3Agc3RvcC1jb2xvcj0iIzIyMiIgb2Zmc2V0PSI1MCUiIC8+CiAgICAgIDxzdG9wIHN0b3AtY29sb3I9IiMzMzMiIG9mZnNldD0iNzAlIiAvPgogICAgPC9saW5lYXJHcmFkaWVudD4KICA8L2RlZnM+CiAgPHJlY3Qgd2lkdGg9IjQwMCIgaGVpZ2h0PSIxNDAiIGZpbGw9IiMzMzMiIC8+CiAgPHJlY3QgaWQ9InIiIHdpZHRoPSI0MDAiIGhlaWdodD0iMTQwIiBmaWxsPSJ1cmwoI2cpIiAvPgogIDxhbmltYXRlIHhsaW5rOmhyZWY9IiNyIiBhdHRyaWJ1dGVOYW1lPSJ4IiBmcm9tPSItNDAwIiB0bz0iNDAwIiBkdXI9IjFzIiByZXBlYXRDb3VudD0iaW5kZWZpbml0ZSIgIC8+Cjwvc3ZnPg=="
         />
       </div>
-      <h2 className="text-xl text-center mt-3">{data.character.name}</h2>
+      <h2 className="text-xl text-center mt-3">{data!.character!.name}</h2>
     </div>
   );
 };
--- src/server/router.ts
+++ src/server/router.ts
@@ -1,20 +1,23 @@
 import { z } from 'zod';
 import * as trpc from '@trpc/server';
 import prisma from '@/server/prisma';
-import { getCharacterById } from '@/server/requests';
 
 export const appRouter = trpc
   .router()
   .query('get-character-by-id', {
     input: z.object({ id: z.string() }),
     async resolve({ input }) {
-      return await getCharacterById(input.id);
+      const id = parseInt(input.id);
+      const result = await prisma.character.findFirst({ where: { id } });
+      return { success: true, character: result };
     },
   })
   .mutation('send-vote', {
     input: z.object({ voteFor: z.string(), voteAgainst: z.string() }),
     async resolve({ input }) {
-      const result = await prisma.vote.create({ data: { ...input } });
+      const votedForId = parseInt(input.voteFor);
+      const votedAgainstId = parseInt(input.voteAgainst);
+      const result = await prisma.vote.create({ data: { votedForId, votedAgainstId } });
       return { success: true, vote: result };
     },
   });
