46ec5c146bc754edfd68e46cc6ab30cb52323198
erikbg7 <erik.bg7@gmail.com>
2022-08-04 11:51:16 +0200

    Improve results page layout

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -24,65 +24,56 @@ const getSortedCharacters = async () => {
 };
 
 const Results: any = ({ characters }: any) => {
-  const parentRef = React.useRef();
+  const parentRef = React.useRef<HTMLDivElement>(null);
 
   const rowVirtualizer = useVirtualizer({
     overscan: 10,
     count: characters.length,
     getScrollElement: () => parentRef.current,
-    estimateSize: () => 86,
+    estimateSize: () => 114,
   });
 
   return (
-    <>
-      {/* The scrollable element for your list */}
-      <div
-        // @ts-ignore
-        ref={parentRef}
-        style={{
-          height: `100vh`,
-          overflow: 'auto', // Make it scroll!
-        }}
-      >
-        {/* The large inner element to hold all of the items */}
+    <div className="flex flex-col items-center p-6">
+      <div className="py-10">
+        <h1 className="text-5xl">Voting Results</h1>
+      </div>
+      <div ref={parentRef} className="w-screen h-[80vh] overflow-auto">
         <div
-          style={{
-            height: `${rowVirtualizer.getTotalSize()}px`,
-            width: '100%',
-            position: 'relative',
-          }}
+          className={`relative w-screen`}
+          style={{ height: `${rowVirtualizer.getTotalSize()}px` }}
         >
-          {/* Only the visible items in the virtualizer, manually positioned to be in view */}
           {rowVirtualizer.getVirtualItems().map((virtualItem) => {
             const item = characters[virtualItem.index];
             return (
               <div
-                className="flex my-2"
+                className="absolute top-0 left-0 w-screen"
                 key={virtualItem.key}
                 style={{
-                  position: 'absolute',
-                  top: 0,
-                  left: 0,
-                  width: '100%',
                   height: `${virtualItem.size}px`,
                   transform: `translateY(${virtualItem.start}px)`,
                 }}
               >
-                <div className="relative w-20 h-20 border-2 rounded-md overflow-hidden cursor-pointer">
-                  <Image src={item.imageUrl as string} alt={item.name} layout="fill" />
-                </div>
-                <div className="flex flex-col ml-3">
-                  <h2 className="text-xl">{item.name}</h2>
-                  <h3 className="text-md">
-                    {item._count.VoteFor} votes for - {item._count.VoteAgainst} votes against
-                  </h3>
+                <div className="flex justify-center p-4 border-b-2 border-b-gray-700">
+                  <div className="relative w-20 h-20 rounded-md overflow-hidden cursor-pointer">
+                    <span className="absolute top-0 left-0 bg-blue-400 z-10 px-1 rounded-br-md shadow-black shadow-md">
+                      {virtualItem.index + 1}
+                    </span>
+                    <Image src={item.imageUrl as string} alt={item.name} layout="fill" />
+                  </div>
+                  <div className="flex flex-col ml-3 w-[300px]">
+                    <h2 className="text-xl">{item.name}</h2>
+                    <h3 className="text-md">
+                      {item._count.VoteFor} votes for - {item._count.VoteAgainst} votes against
+                    </h3>
+                  </div>
                 </div>
               </div>
             );
           })}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
