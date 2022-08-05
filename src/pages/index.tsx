ff370b64577008d30b8bf21205da735e27222246
erikbg7 <erik.bg7@gmail.com>
2022-08-05 13:15:01 +0200

    Use image placeholders as utils constant

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,9 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import { trpc } from '@/utils/trpc';
-import { getCharactersIds } from '@/utils/characters';
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import { BASE64_PLACEHOLDER } from '@/utils/images';
+import { getCharactersIds } from '@/utils/characters';
 
 const Home: NextPage = () => {
   const [characterIds, setCharacterIds] = useState<string[]>(getCharactersIds);
@@ -100,7 +101,7 @@ const CharacterCard: React.FC<CharacterCardProps> = ({ id, onClick }) => {
           src={data!.character!.imageUrl}
           layout="fill"
           placeholder="blur"
-          blurDataURL="data:image/svg+xml;base64,Cjxzdmcgd2lkdGg9IjQwMCIgaGVpZ2h0PSIxNDAiIHZlcnNpb249IjEuMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayI+CiAgPGRlZnM+CiAgICA8bGluZWFyR3JhZGllbnQgaWQ9ImciPgogICAgICA8c3RvcCBzdG9wLWNvbG9yPSIjMzMzIiBvZmZzZXQ9IjIwJSIgLz4KICAgICAgPHN0b3Agc3RvcC1jb2xvcj0iIzIyMiIgb2Zmc2V0PSI1MCUiIC8+CiAgICAgIDxzdG9wIHN0b3AtY29sb3I9IiMzMzMiIG9mZnNldD0iNzAlIiAvPgogICAgPC9saW5lYXJHcmFkaWVudD4KICA8L2RlZnM+CiAgPHJlY3Qgd2lkdGg9IjQwMCIgaGVpZ2h0PSIxNDAiIGZpbGw9IiMzMzMiIC8+CiAgPHJlY3QgaWQ9InIiIHdpZHRoPSI0MDAiIGhlaWdodD0iMTQwIiBmaWxsPSJ1cmwoI2cpIiAvPgogIDxhbmltYXRlIHhsaW5rOmhyZWY9IiNyIiBhdHRyaWJ1dGVOYW1lPSJ4IiBmcm9tPSItNDAwIiB0bz0iNDAwIiBkdXI9IjFzIiByZXBlYXRDb3VudD0iaW5kZWZpbml0ZSIgIC8+Cjwvc3ZnPg=="
+          blurDataURL={BASE64_PLACEHOLDER}
         />
       </div>
       <h2 className="text-xl text-center mt-3">{data!.character!.name}</h2>
