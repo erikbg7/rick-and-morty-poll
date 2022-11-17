84ea2b27ee47ab1ef5fed5ce559ace7db5254351
erikbg7 <erik.bg7@gmail.com>
2022-11-17 10:32:05 +0100

    Test 2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,7 @@
-import { withTRPC } from '@trpc/next';
-import { AppType } from 'next/dist/shared/lib/utils';
 import '@/styles/globals.css';
-import { AppRouter } from '@/server/router';
 
 const MyApp = ({ Component, pageProps }: any) => {
   return <Component {...pageProps} />;
-};
-
-MyApp.getInitialProps = () => {
-  return { title: 'title' };
 };
 
 export default MyApp;
